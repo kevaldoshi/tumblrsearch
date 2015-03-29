b7ac24281cbbdfcc371c1adcdee9980964dd7074
keval <kevdoshi@gmail.com>
2015-03-29 11:32:13 +0000

    removed unused code

--- tumblrsearch/settings.py
+++ tumblrsearch/settings.py
@@ -23,7 +23,7 @@ STATICFILES_DIRS = (
 )
 
 def get_env_variable(var_name):
-    """Get the environment variable or return exception"""
+
     try:
         return os.environ[var_name]
     except KeyError:
